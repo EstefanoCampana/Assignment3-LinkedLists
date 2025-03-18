87d14f8f6d615f701f22ea84e01ca02b977cea25
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-03-18 11:07:46 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
