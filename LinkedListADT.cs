cdbcc59e4ca13177a62d708d9330b3401e2bbd3a
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-03-24 12:09:48 -0600

    Adds a summary to all methods.

--- LinkedListADT.cs
+++ LinkedListADT.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,56 +9,78 @@ namespace Assignment3_LinkedLists
 {
     public interface LinkedListADT
     {
-        //Checks if the list is empty.
-        //return True if it is empty.
+        /// <summary>
+        /// Checks if the list is empty.
+        /// </summary>
+        /// <returns>True if it is empty.</returns>
         bool IsEmpty();
 
-        //Clears the list.
+        ///<summary>Clears the list.</summary>
         void Clear();
 
-        //Adds to the end of the list.
-        //Object data to append.
+        /// <summary>
+        /// Adds to the end of the list.
+        /// </summary>
+        /// <param name="data">Data to append.</param>
         void Append(Object data);
 
-        //Prepends (adds to beginning) data to the list.
-        //parameter Object Data to store inside element.
+        /// <summary>
+        /// Prepends (adds to beginning) data to the list.
+        /// </summary>
+        /// <param name="data">Data to store inside element.</param>
         void Prepend(Object data);
 
-        //Adds a new element at a specific position.
-        //parameter data Data that element is to contain.
-        //parameter index Index to add new element at.
-        //throws exception IndexOutOfRangeException Thrown if index is negative or past the size of the list.
+        /// <summary>
+        /// Adds a new element at a specific position.
+        /// </summary>
+        /// <param name="data">Data that element is to contain.</param>
+        /// <param name="index">Index to add new element at.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown if index is negative or past the size of the list.</exception>
         void Insert(Object data, int index);
 
-        //Replaces the data  at index.
-        //parameter data Data to replace.
-        //parameter index Index of element to replace.
-        //throws IndexOutOfRangeException Thrown if index is negative or larger than size - 1 of list.
-        void Replace(Object data, int index);
-
-        //Gets the number of elements in the list.
-        //return Size of list (0 meaning empty)
+        /// <summary>
+        /// Replaces the data at index.
+        /// </summary>
+        /// <param name="data">Data to replace.</param>
+        /// <param name="index">Index of element to replace.</param>
+        /// <exception cref="IndexOutOfRangeException">Thrown if index is negative or larger than size - 1 of list.</exception>
+        void Replace(Object data, int index)
+        {
+            
+        }
+        /// <summary>
+        /// Gets the number of elements in the list.
+        /// </summary>
+        /// <returns>Size of list (0 meaning empty).</returns>
         int Size();
 
-        //Removes element at index from list, reducing the size.
-        //parameter index Index of element to remove.
-        //throws exception IndexOutOfBoundsException Thrown if index is negative or past the size - 1.
+        /// <summary>
+        /// Removes element at index from list, reducing the size.
+        /// </summary>
+        /// <param name="index">Index of element to remove.</param>
+        /// <exception cref="IndexOutOfBoundsException">Thrown if index is negative or past the size - 1.</exception>
         void Delete(int index);
 
-        //Gets the data at the specified index.
-        //parameter index Index of element to get.
-        //return Data in element or null if it was not found.
-        //throws exception IndexOutOfRangeException Thrown if index is negative or larger than size - 1 of the list.
+        /// <summary>
+        /// Gets the data at the specified index.
+        /// </summary>
+        /// <param name="index">Index of element to get.</param>
+        /// <returns> Data in element or null if it was not found.</returns>
+        /// <exception cref="IndexOutOfRangeException">Thrown if index is negative or larger than size - 1 of the list.</exception>
         Object Retrieve(int index);
 
-        //Gets the first index of element containing data.
-        //parameter data Data object to find the first index of.
-        //return First of index of element with matching data or -1 if not found.
+        /// <summary>
+        /// Gets the first index of element containing data.
+        /// </summary>
+        /// <param name="data">Data object to find the first index of.</param>
+        /// <returns>First of index of element with matching data or -1 if not found.</returns>
         int IndexOf(Object data);
 
-        //Go through elements and check if we have one with data.
-        //parameter data Data object to search for.
-        //return True if element exists with value.
+        /// <summary>
+        /// Go through elements and check if we have one with data.
+        /// </summary>
+        /// <param name="data">Data object to search for.</param>
+        /// <returns>True if element exists with value.</returns>
         bool Contains(Object data);
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,59 +8,59 @@ namespace LinkedLists
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Function [PRINT]");
-            list.PrintData();
-
-            Console.WriteLine("Function [Append] 'a'");
-            list.Append('a');
-            list.PrintList();
-
-            Console.WriteLine("Function [Append] 'b'");
-            list.Append('b');
-            list.PrintList();
-
-            Console.WriteLine("Function [Append] 'c'");
-            list.Append('c');
-            list.PrintData();
-
-            Console.WriteLine("Function [Retrieve] '0' ");
-            Console.WriteLine(list.Retrieve(0));
-
-            Console.WriteLine("Function [Retrieve] '1' ");
-            Console.WriteLine(list.Retrieve(1));
-
-            Console.WriteLine("Function [Retrieve] '2' ");
-            Console.WriteLine(list.Retrieve(2));
-            list.PrintData();
-
-            Console.WriteLine("\n\nFunction [Insert] 'd' at '0'");
-            list.Insert('d', 0);
-            list.PrintList();
-
-            Console.WriteLine("Function [Insert] '1' at '1'");
-            list.Insert('1', 1);
-            list.PrintList();
-
-            Console.WriteLine("Function [Insert] '2' at '2'");
-            list.Insert('2', 2);
-            list.PrintData();
-
-            //Console.WriteLine("Function [Replace] 'c' with 'e'");
-            //list.Replace('e', 2);
+            //Console.WriteLine("Function [PRINT]");
             //list.PrintData();
 
-            //Console.WriteLine("Function [Delete] at index 2");
-            //list.Delete(2);
+            //Console.WriteLine("Function [Append] 'a'");
+            //list.Append('a');
+            //list.PrintList();
+
+            //Console.WriteLine("Function [Append] 'b'");
+            //list.Append('b');
+            //list.PrintList();
+
+            //Console.WriteLine("Function [Append] 'c'");
+            //list.Append('c');
             //list.PrintData();
 
+            //Console.WriteLine("Function [Retrieve] '0' ");
+            //Console.WriteLine(list.Retrieve(0));
 
-            //Console.WriteLine("Function [FindIndexOf] 'b'");
-            //Console.WriteLine("Index found: " + list.IndexOf('b') + "\n\n");
+            //Console.WriteLine("Function [Retrieve] '1' ");
+            //Console.WriteLine(list.Retrieve(1));
 
-
-            //Console.WriteLine("Function [Clear]");
-            //list.Clear();
+            //Console.WriteLine("Function [Retrieve] '2' ");
+            //Console.WriteLine(list.Retrieve(2));
             //list.PrintData();
+
+            //Console.WriteLine("\n\nFunction [Insert] 'd' at '0'");
+            //list.Insert('d', 0);
+            //list.PrintList();
+
+            //Console.WriteLine("Function [Insert] '1' at '1'");
+            //list.Insert('1', 1);
+            //list.PrintList();
+
+            //Console.WriteLine("Function [Insert] '2' at '2'");
+            //list.Insert('2', 2);
+            //list.PrintData();
+
+            Console.WriteLine("Function [Replace] 'c' with 'e'");
+            list.Replace('e', 2);
+            list.PrintData();
+
+            Console.WriteLine("Function [Delete] at index 2");
+            list.Delete(2);
+            list.PrintData();
+
+
+            Console.WriteLine("Function [FindIndexOf] 'b'");
+            Console.WriteLine("Index found: " + list.IndexOf('b') + "\n\n");
+
+
+            Console.WriteLine("Function [Clear]");
+            list.Clear();
+            list.PrintData();
 
             Console.WriteLine("Function [Size]");
             list.Size();
